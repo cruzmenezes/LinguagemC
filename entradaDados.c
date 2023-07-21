3f16a7422985c5f1e74d19707caebfa8d059c4b3
Edson Victor da cruz Menezes <91231848+cruzmenezes@users.noreply.github.com>
2023-07-21 15:07:03 +0000

    aprendendo linguagem c

---
+++ entradaDados.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+  int primeira = 30;
+  int segunda = 10;
+  int soma = primeira + segunda;
+
+  printf("%d, A soma dos numeros são:" soma  );
+  return 0;
+}
---
+++ helloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Olá Mundo!!!!\n");
+  printf("Vamos programar em C!!!!\n");
+  return 0;
+}
