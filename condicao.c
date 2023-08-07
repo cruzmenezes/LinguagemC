86b1a49bc6b2006001a0278a2ca619f6ce6cccb9
Edson Victor da cruz Menezes <91231848+cruzmenezes@users.noreply.github.com>
2023-08-07 20:58:45 +0000

    condicao

---
+++ condicao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//variavel 
+float m;
+
+int main(){
+    printf("insira a nota: \n");
+    scanf("%f", &m);
+
+    if (m >= 7.0){
+        printf("aprovado(a)! \n);
+    }
+}
